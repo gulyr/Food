2b2f7b5768a242df07109ae5994d7f4ba5c381b1
Gul Rasulzada <guldastayr@code.edu.az>
2024-11-05 17:17:52 +0400

    refactor: "Food updated"

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import dataReducer from "./slices/slice";
-import uiReducer from "./slices/uiSlice";
+import dataReducer from "./slices/slice.js";
+import uiReducer from "./slices/uiSlice.js";
 
 export const store = configureStore({
   reducer: {
